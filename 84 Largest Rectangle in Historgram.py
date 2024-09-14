60dbe1ab1726de92d0eff7f397dd87dc994172f4
Myrrolinz <mildred1195564913@gmail.com>
2024-09-14 00:07:16 -0400

    8.13

---
+++ 84 Largest Rectangle in Historgram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        heights.insert(0, 0)
+        heights.append(0)
+        stack = [0]
+        result = 0
+        for i in range(1, len(heights)):
+            while stack and heights[i] < heights[stack[-1]]:
+                mid_height = heights[stack[-1]]
+                stack.pop()
+                if stack:
+                    # area = width * height
+                    area = (i - stack[-1] - 1) * mid_height
+                    result = max(area, result)
+            stack.append(i)
+        return result
